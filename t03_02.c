d6f2823c321b78213058e2ac3d257a3f3498cfdf
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 19:32:33 +0700

    chyntia
    ya

--- t03_02.c
+++ t03_02.c
@@ -6,9 +6,9 @@ int main() {
     int input_num;
     int min_val = INT_MAX;
     int max_val = INT_MIN;
-    int first_num = 0;
-    int second_num = 0;
-    double average_first_two = 0.0;
+    int third_num = 0;
+    int fourth_num = 0;
+    double average_third_fourth = 0.0;
 
     // Meminta input n sampai n positif
     do {
@@ -21,27 +21,32 @@ int main() {
             scanf("%d", &input_num);
         } while (input_num < -100 || input_num > 100);
 
-        if (i == 0) {
-            first_num = input_num;
-        } else if (i == 1) {
-            second_num = input_num;
+        // Simpan angka ke-3 dan ke-4
+        if (i == 2) {
+            third_num = input_num;
+        } else if (i == 3) {
+            fourth_num = input_num;
         }
 
+        // Cari minimum
         if (input_num < min_val) {
             min_val = input_num;
         }
+
+        // Cari maksimum
         if (input_num > max_val) {
             max_val = input_num;
         }
     }
 
-    if (n >= 2) {
-        average_first_two = (double)(first_num + second_num) / 2.0;
+    // Hitung rata-rata jika minimal ada 4 angka
+    if (n >= 4) {
+        average_third_fourth = (double)(third_num + fourth_num) / 2.0;
     }
 
     printf("%d\n", min_val);
     printf("%d\n", max_val);
-    printf("%.2f\n", average_first_two);
+    printf("%.2f\n", average_third_fourth);
 
     return 0;
 }
--- t03_03.c
+++ t03_03.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     int n;
-    scanf("%d", &n);
+    int input_num;
+    int min_val = INT_MAX;
+    int max_val = INT_MIN;
+    int first_num = 0;
+    int second_num = 0;
+    int third_num = 0;
+    int fourth_num = 0;
+    double average_first_two = 0.0;
+    double average_third_fourth = 0.0;
 
-    int angka, prev;
-    int min, max;
-    int total = 0;
+    do {
+        scanf("%d", &n);
+    } while (n <= 0);
 
-    // baca angka pertama
-    scanf("%d", &angka);
-    min = max = angka;
-    total += angka;
-    prev = angka;
-
-    double max_avg_pair = -1000.0;  // nilai awal kecil
-
-    // baca sisa angka
-    for(int i = 1; i < n; i++) {
-        scanf("%d", &angka);
-
-        // cari min & max
-        if(angka < min)
-            min = angka;
-        if(angka > max)
-            max = angka;
-
-        total += angka;
-
-        // hitung rata-rata pasangan berurutan
-        double avg_pair = (prev + angka) / 2.0;
-        if(avg_pair > max_avg_pair)
-            max_avg_pair = avg_pair;
-
-        prev = angka;  // simpan untuk iterasi berikutnya
+    if (n < 4) {
+        return 1;
     }
 
-    double avg_total = total / (double)n;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &input_num);
 
-    printf("%d\n", min);
-    printf("%d\n", max);
-    printf("%.2f\n", avg_total);
-    printf("%.2f\n", max_avg_pair);
+        if (i == 0) {
+            first_num = input_num;
+        } else if (i == 1) {
+            second_num = input_num;
+        } else if (i == 2) {
+            third_num = input_num;
+        } else if (i == 3) {
+            fourth_num = input_num;
+        }
+
+        if (input_num < min_val) {
+            min_val = input_num;
+        }
+        if (input_num > max_val) {
+            max_val = input_num;
+        }
+    }
+
+    average_first_two = (double)(first_num + second_num) / 2.0;
+    average_third_fourth = (double)(third_num + fourth_num) / 2.0;
+
+    printf("%d\n", min_val);
+    printf("%d\n", max_val);
+    printf("%.2f\n", average_first_two);
+    printf("%.2f\n", average_third_fourth);
 
     return 0;
 }
