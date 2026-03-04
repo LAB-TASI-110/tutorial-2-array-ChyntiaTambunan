e0f08a0d0e63330017755e4b3d23cd92e7abc500
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 19:22:39 +0700

    Update t03_02.c

--- t03_02.c
+++ t03_02.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <limits.h>
+#include <limits.h> // Untuk INT_MAX dan INT_MIN
 
 int main() {
     int n;
@@ -8,30 +8,23 @@ int main() {
     int max_val = INT_MIN;
     int first_num = 0;
     int second_num = 0;
-    int third_num = 0;
-    int fourth_num = 0;
     double average_first_two = 0.0;
-    double average_third_fourth = 0.0;
 
+    // Meminta input n sampai n positif
     do {
         scanf("%d", &n);
     } while (n <= 0);
 
-    if (n < 4) {
-        return 1;
-    }
-
     for (int i = 0; i < n; i++) {
-        scanf("%d", &input_num);
+        // Meminta input_num sampai input_num berada dalam rentang -100 hingga 100
+        do {
+            scanf("%d", &input_num);
+        } while (input_num < -100 || input_num > 100);
 
         if (i == 0) {
             first_num = input_num;
         } else if (i == 1) {
             second_num = input_num;
-        } else if (i == 2) {
-            third_num = input_num;
-        } else if (i == 3) {
-            fourth_num = input_num;
         }
 
         if (input_num < min_val) {
@@ -40,14 +33,15 @@ int main() {
         if (input_num > max_val) {
             max_val = input_num;
         }
-    } 
+    }
 
-    average_first_two = (double)(first_num + second_num) / 2.0;
+    if (n >= 2) {
+        average_first_two = (double)(first_num + second_num) / 2.0;
+    }
 
     printf("%d\n", min_val);
     printf("%d\n", max_val);
     printf("%.2f\n", average_first_two);
-    
 
     return 0;
 }
