af68bb797894071229b0f96298e000665a7293ff
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-03-04 19:25:39 +0700

    chyn
    ya

--- t03_01.c
+++ t03_01.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-#include <limits.h> // Untuk INT_MAX dan INT_MIN
 
 int main() {
     int n;
-    int input_num;
-    int min_val = INT_MAX;
-    int max_val = INT_MIN;
-
-    // Meminta input n sampai n positif
-    do {
-        scanf("%d", &n);
-    } while (n <= 0);
-
-    for (int i = 0; i < n; i++) {
-        // Meminta input_num sampai input_num berada dalam rentang -100 hingga 100
-        do {
-            scanf("%d", &input_num);
-        } while (input_num < -100 || input_num > 100);
-
-        if (input_num < min_val) {
-            min_val = input_num;
+    int angka;
+    int min, max;
+    
+    // Membaca jumlah data
+    scanf("%d", &n);
+    
+    // Membaca angka pertama sebagai nilai awal min dan max
+    scanf("%d", &angka);
+    min = angka;
+    max = angka;
+    
+    // Membaca sisa angka
+    for(int i = 1; i < n; i++) {
+        scanf("%d", &angka);
+        
+        if(angka < min) {
+            min = angka;
         }
-        if (input_num > max_val) {
-            max_val = input_num;
+        
+        if(angka > max) {
+            max = angka;
         }
     }
-
-    printf("%d\n", min_val);
-    printf("%d\n", max_val);
-
+    
+    // Menampilkan hasil
+    printf("%d\n", min);
+    printf("%d\n", max);
+    
     return 0;
 }
